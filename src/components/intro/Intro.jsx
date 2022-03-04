ed8bce73d5e1dab4d11062e4a237d7720405b2f9
jsncorn <jsncourn@gmail.com>
2022-03-03 18:48:29 -0800

    begin intro, redid selfie to look better

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,8 +4,16 @@ import "./intro.css"
 export default function Intro() {
   return (
     <div className='intro' id='intro'>
-      <div className="introLeft"></div>
-      <div className="introRight"></div>
+      <div className="introLeft">
+        <div className="selfie">
+          <img src="../assets/selfie.JPG" alt="selfie"></img>
+          </div>
+        </div>
+      <div className="introRight">
+        <div className="introWrap">
+          <h2>Hello my name is</h2>
+          </div>
+        </div>
       </div>
   )
 }
