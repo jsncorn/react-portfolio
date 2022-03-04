f6a929467e6b1d920c70384da5c14ca787c81fdb
jsncorn <jsncourn@gmail.com>
2022-03-03 22:16:56 -0800

    added regex vlaidate email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./contact.css"
 import { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 export default function Contact() {
   const [form, setForm] = useState({name: '', email: '', subject: '', content: ''});
