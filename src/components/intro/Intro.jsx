714554426c642872868c4068e2dda978136cfeac
jsncorn <jsncourn@gmail.com>
2022-03-03 18:35:48 -0800

    added divs for left and right, added flex, and added new asset image

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,9 @@ import "./intro.css"
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'>Intro</div>
+    <div className='intro' id='intro'>
+      <div className="introLeft"></div>
+      <div className="introRight"></div>
+      </div>
   )
 }
