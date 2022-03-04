e5fbfbe4619b91b49ed725e84688f25988083d6d
jsncorn <jsncourn@gmail.com>
2022-03-03 19:51:08 -0800

    put titles as overlay, added demo photos, made the boxes bigger, added spacing

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -8,35 +8,35 @@ export default function Gallery() {
       <div className="galleryContainer">
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="https://github.com/jsncorn/portfolio-v3/blob/main/public/photos/Screenshot%202022-02-18%20165100.png?raw=true" alt="" />
           <h3>Project Title</h3>
         </div>
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="https://github.com/imanmansour86/get-moving/raw/main/public/images/homepage.gif" alt="" />
           <h3>Project Title</h3>
         </div>
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="https://user-images.githubusercontent.com/87446864/150031399-87540499-0212-409a-8040-2317b3085ac0.gif" alt="" />
           <h3>Project Title</h3>
         </div>
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="../assets/ecommerce-demo.png" alt="" />
           <h3>Project Title</h3>
         </div>
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="../assets/social-network-code.png" alt="" />
           <h3>Project Title</h3>
         </div>
 
         <div className="item">
-          <img src="" alt="" />
+          <img src="../assets/JS-quiz-demo.png" alt="" />
           <h3>Project Title</h3>
         </div>
-        
+
       </div>
     </div>
   )
