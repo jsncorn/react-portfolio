35cc882caaf42dc8e28c3de5c84350609c5917d3
jsncorn <jsncourn@gmail.com>
2022-03-03 19:27:37 -0800

    added typing feature

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import "./intro.css"
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ["Node.js", "React.js", "Express.js", "Fullstack"]
+    });
+  }, [])
+
   return (
     <div className='intro' id='intro'>
       <div className="introLeft">
         <div className="selfie">
           <img src="../assets/selfie.JPG" alt="selfie"></img>
-          </div>
         </div>
+      </div>
       <div className="introRight">
         <div className="introWrap">
           <h2>Hello! I'm</h2>
           <h1>Jason Ngo</h1>
-          <h3></h3>
-          </div>
-          <div className='arrowDiv'>
+          <h3><span ref={textRef}></span> Developer</h3>
+        </div>
+        <div className='arrowDiv'>
           <a className='anchorArrow' href="#portfolio">
-          <img className='downArrow' src="../assets/down.png" alt="down arrow"/>
-        </a>
-         </div>
+            <img className='downArrow' src="../assets/down.png" alt="down arrow" />
+          </a>
         </div>
       </div>
+    </div>
   )
 }
