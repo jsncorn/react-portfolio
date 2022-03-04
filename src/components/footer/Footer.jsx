7a44acb099a08879287260faa6ce9bc83e3526b0
jsncorn <jsncourn@gmail.com>
2022-03-03 17:40:15 -0800

    added more packages for icons, added icons to footer with href

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import "./footer.css"
+import React from 'react';
+import "./footer.css";
+import { GitHub, Email, LinkedIn, PhoneIphone } from "@mui/icons-material";
 
 export default function Footer() {
   return (
-    <div className="footer" >Footer</div>
+    <div className="footer" >
+      <div className="footer-wrapper">
+        <a href='https://www.linkedin.com/in/jason-ngo-050b481b3/' className="icon">
+          <LinkedIn />
+        </a>
+        <a href='https://www.github.com/jsncorn' className="icon">
+          <GitHub />
+        </a>
+        <a href="mailto:jsncourn@gmail.com" className="icon">
+          <Email />
+        </a>
+        <a href="tel:+15107105041" className="icon">
+          <PhoneIphone />
+        </a>
+      </div>
+    </div>
   )
 }
