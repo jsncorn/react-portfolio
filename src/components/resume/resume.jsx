49e20d1440a61bd0b8405220a12fc31042b4bc3d
jsncorn <jsncourn@gmail.com>
2022-03-04 11:58:08 -0800

    fixed overflow on resume div

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Resume from './resume.pdf';
+import './resume.css';
 
 export default function resume() {
   return (
-    <div id='resume'>
+    <div id='resume' class='resume'>
       <iframe title='resume' src={Resume} width="100%" height="100%" />
     </div>
   )
