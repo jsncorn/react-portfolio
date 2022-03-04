95eaeea35ff7f62731967964e43dd4c6c12e6c17
jsncorn <jsncourn@gmail.com>
2022-03-03 16:39:40 -0800

    add topbar to main page, separate styling

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-
-const topbarStyles = {
-    main: {
-        backgroundColor: "#00008B",
-        width: "100%",
-        height: "70px",
-    }
-}
+import React from 'react';
+import "./topbar.css"
 
 export default function Topbar() {
   return (
-    <div styles={topbarStyles.main}>Topbar</div>
+    <div className="topbar">
+        Topbar
+        </div>
   )
 }
