0bf5998dc7a4540e917ae7a049b32bbd695fef3f
jsncorn <jsncourn@gmail.com>
2022-03-03 21:52:28 -0800

    added contact form with no styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,11 +5,22 @@ export default function Contact() {
   return (
     <div className='contact' id='contact'>
       <div className="contactLeft">
-        <img src="../assets/networking.jpg" alt="network web"/>
-        </div>
-      <div className="contactRight">
-        
-        </div>
+        <img src="../assets/networking.jpg" alt="network web" />
       </div>
+      <div className="contactRight">
+        <h2>Get In Touch With Me</h2>
+        <form>
+          <label for="name">Your Name</label><br />
+          <input type="text" id="name" placeholder='First and Last' /><br />
+          <label for="email">Email</label><br />
+          <input type="email" id="email" placeholder="Email" /><br />
+          <label for="subject">Subject</label><br />
+          <input type="subject" id="subject" placeholder="Subject" /><br />
+          <label for="content">Message</label><br />
+          <textarea name="content" id="content" cols="20" rows="4" placeholder="Message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
