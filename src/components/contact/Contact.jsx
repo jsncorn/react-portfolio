3879d21593df81e7e8f32e9603faddeefd8e8286
jsncorn <jsncourn@gmail.com>
2022-03-03 21:43:56 -0800

    added image for contact form; split half and half

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,8 +4,12 @@ import "./contact.css"
 export default function Contact() {
   return (
     <div className='contact' id='contact'>
-      <div className="contactLeft"></div>
-      <div className="contactRight"></div>
+      <div className="contactLeft">
+        <img src="../assets/networking.jpg" alt="network web"/>
+        </div>
+      <div className="contactRight">
+        
+        </div>
       </div>
   )
 }
